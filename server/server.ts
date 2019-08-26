f0874b9bc80fc342deff744bf6ca9486af6d50c2
DavidCardenas1 <cdcardenas@tekmexico.com>
2019-08-26 12:54:34 -0700

    Init commit

--- server/server.ts
+++ server/server.ts
@@ -3,11 +3,12 @@ import path = require('path');
 import express = require('express');
 
 const publicPath:string = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
 let app = express();
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000');
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
 });
 
 // console.log(publicPath);
